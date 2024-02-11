26a099256a681f03a6f1679f5ea44b6298a3b377
SekaliPakaiBuang <99380177+SekaliPakaiBuang@users.noreply.github.com>
2024-02-11 22:26:08 +0700

    Timer sudah bisa jalan (dengan overclocknya)

--- js/init.js
+++ js/init.js
@@ -9,29 +9,14 @@ const maxTimeInput = document.querySelector("input[name=max_time]");
 const maxSpeedInput = document.querySelector("input[name=max_speed]");
 
 // Input override
-const addTimeInput = document.querySelector("input[name=add_time]");
-const addSpeedInput = document.querySelector("input[name=add_speed]");
-
-// Tombol
-const increaseTimeBtn = document.querySelector("#btn__time-increase");
-const decreaseTimeBtn = document.querySelector("#btn__time-decrease");
-
-const increaseSpeedBtn = document.querySelector("#btn__speed-increase");
-const decreaseSpeedBtn = document.querySelector("#btn__speed-decrease");
-
-const playBtn = document.querySelector("#btn__timer-play");
-const pauseBtn = document.querySelector("#btn__timer-pause");
-const resetBtn = document.querySelector("#btn__timer-reset");
-
-const timerSettingSaveBtn = document.querySelector("#btn__save-timer");
+const modifyTimeInput = document.querySelector("input[name=add_time]");
+const modifySpeedInput = document.querySelector("input[name=add_speed]");
 
 // Inisialisasi variable
 if (typeof localStorage.initialTime === "undefined") localStorage.initialTime = 3600;
 if (typeof localStorage.initialSpeed === "undefined") localStorage.initialSpeed = 1;
 if (typeof localStorage.time === "undefined") localStorage.time = localStorage.initialTime;
 if (typeof localStorage.speed === "undefined") localStorage.speed = localStorage.initialSpeed;
-
-let isRunning = false;
 
 timeLabel.textContent = localStorage.time;
 speedLabel.textContent = localStorage.speed;
--- js/logic.js
+++ js/logic.js
@@ -1,15 +1,70 @@
 // Logic tombol
-timerSettingSaveBtn.addEventListener("click", () => {
-    let inputTime = initialTimeInput.valueAsNumber
-    let inputSpeed = initialSpeedInput.valueAsNumber
+function saveTimerSettings() {
+    const inputTime = Math.trunc(initialTimeInput.valueAsNumber);
+    const inputSpeed = Math.trunc(initialSpeedInput.valueAsNumber * 100) / 100;
 
     if (inputTime > 0 && inputSpeed > 0) {
         localStorage.initialTime = inputTime;
-        localStorage.initialSpeedInput = inputSpeed;
+        localStorage.initialSpeed = inputSpeed;
+
+        alert("Successfully saved");
     }
-    else {
-        alert("Initial Time & Initial Speed can't be empty or zero")
+    else alert("Initial Time & Initial Speed should be greater than zero");
+}
+
+function playTimer() {
+    if (!isRunning) {
+        isRunning = true;
+        clock = setInterval(onClockTick, 1000 / Number(localStorage.speed));
     }
-});
+}
+
+function pauseTimer() {
+    isRunning = false;
+    clearInterval(clock);
+}
+
+function resetTimer() {
+    isRunning = false;
+    clearInterval(clock);
+
+    localStorage.time = localStorage.initialTime;
+    localStorage.speed = localStorage.initialSpeed;
+
+    timeLabel.textContent = localStorage.time;
+    speedLabel.textContent = localStorage.speed;
+
+}
+
+function addTime() {
+    const input = Math.trunc(modifyTimeInput.valueAsNumber);
+    localStorage.time = Number(localStorage.time) + input;
+
+    if (localStorage.time <= 0) {
+        isRunning = false;
+        localStorage.time = 0;
+        clearInterval(clock);
+    }
+
+    timeLabel.textContent = localStorage.time;
+}
+
+function addSpeed() {
+    // TODO: Tambah kecepatan timer (awas rounding error)
+}
 
 // Logic timer
+let clock;
+let isRunning = false;
+
+function onClockTick() {
+    localStorage.time -= 1;
+
+    if (localStorage.time <= 0) {
+        isRunning = false;
+        localStorage.time = 0;
+        clearInterval(clock);
+    }
+
+    timeLabel.textContent = localStorage.time;
+}
