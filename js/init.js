1308051e003b565a6ccac2b97b1802f61d793dd8
SekaliPakaiBuang <99380177+SekaliPakaiBuang@users.noreply.github.com>
2024-02-19 21:53:31 +0700

    Kembalikan ke ceil

--- js/init.js
+++ js/init.js
@@ -53,7 +53,7 @@ if (channelID == null) throw Error("Trakteer Channel ID is not set. Adding ?id= 
 // Fungsi global
 function styleTime(time) {
     let sign = time < 0 ? "-" : "";
-    let seconds = Math.trunc(Math.abs(time));
+    let seconds = Math.ceil(Math.abs(time));
     if (time == 0) return "0s";
 
     let days = Math.floor(seconds / (24 * 60 * 60));
